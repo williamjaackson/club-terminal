2d9db59dd8e1d4921e0a5bdd5e4a5e34415f4196
William Jackson <wcjaackson@gmail.com>
2025-04-12 13:49:37 +1000

    fix login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+
 export default function LoginPage() {
-  return <div>You cannot access this resource unless you are signed in.</div>;
+  const { user } = useAuth();
+
+  if (!user) {
+    return <div>You cannot access this resource unless you are signed in.</div>;
+  }
+
+  return <div>You are signed in.</div>;
 }
